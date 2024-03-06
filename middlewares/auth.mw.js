8723eaee8f4ff02124b6d98b3118073617d66bfc
Saikat-Adhya <saikatadhya1@gmail.com>
2024-03-06 15:46:46 +0530

    added category section with only admin permission

--- middlewares/auth.mw.js
+++ middlewares/auth.mw.js
@@ -1,4 +1,6 @@
 const user_model = require("../models/user.model")
+const jwt = require("jsonwebtoken")
+const auth_config = require("../configs/auth.config")
 
 /**
  * Create a mw will check if the req body is proper and correct!
@@ -67,7 +69,56 @@ const verifySigninBody = async(req,res,next)=>{
 
 
 }
+const verifyToken = (req, res, next)=>{
+    //check if the token is present in the header
+    const token = req.headers['x-access-token']
+
+    if(!token){
+        return res.status(403).send({
+            message : "No token found : UnAuthorized"
+        })
+    }
+
+
+    //If it's the valid token
+    jwt.verify(token, auth_config.secret,async (err, decoded)=>{
+        if(err){
+            return res.status(401).send({
+                message : "UnAuthorized !"
+            })
+        }
+        const user = await user_model.findOne({userId : decoded.id})
+        if(!user){
+            return res.status(400).send({
+                message : "UnAuthorized, this user for this token doesn't exist"
+            })
+        }
+        //Set the user info in the req body
+        req.user = user
+        next()
+    })
+   
+
+    
+    //Then move to the next step
+
+
+}
+
+const isAdmin = (req, res, next) =>{
+    const user = req.user
+    if(user && user.userType == "ADMIN"){
+        next()
+    }else{
+        return res.status(403).send({
+            message : "Only ADMIN users are allowed to access this endpoint"
+        })
+    }
+
+}
 module.exports = {
     verifySignupBody : verifySignupBody,
-    verifySigninBody : verifySigninBody
+    verifySigninBody : verifySigninBody,
+    verifyToken : verifyToken,
+    isAdmin : isAdmin
 }
--- routes/category.routes.js
+++ routes/category.routes.js
@@ -1,8 +1,11 @@
+const authMw = require("../middlewares/auth.mw")
+
 /**
  * POST localhost:8080/ecomm/api/v1/auth/categories
  */
 category_controller = require("../controllers/category.controller")
+auth_mw = require("../middlewares/auth.mw")
 
 module.exports = (app)=>{
-    app.post("/ecomm/api/v1/auth/categories",category_controller.createNewCategory)
+    app.post("/ecomm/api/v1/auth/categories",[auth_mw.verifyToken, authMw.isAdmin],category_controller.createNewCategory)
 }
