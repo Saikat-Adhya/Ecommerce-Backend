8c10df2b785c427049d6b08452a10dc394a6c71c
Saikat-Adhya <saikatadhya1@gmail.com>
2024-03-26 11:19:01 +0530

    Added Create,update,delete,show and added nodemon

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -40,3 +40,63 @@ exports.createNewCategory = async(req, res)=>{
     //Return the response of created category
 
 }
+//---------------------------------------------------------------//
+//This is use to delete the category.//
+
+exports.deleteCategory= async(req, res) => {
+    try {
+        // const cat_data = {
+        //     name : req.body.name,
+        //     description : req.body.description
+        // }
+        const {id} = req.params;
+
+        const product = await category_model.findByIdAndDelete(id);
+
+        if (!product) {
+            return res.status(404).json({message: "Oops!! Product not found"})
+            
+        } else {
+            
+            res.status(200).json({message: "Product deleted successfully !"});
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message})
+        
+    }
+
+}
+//----------------------------------------------------//
+//This is use to update the category.//
+
+exports.updateCategory= async(req, res)=>{
+    try {
+        const {id} = req.params;
+
+        const product=await category_model.findByIdAndUpdate(id,req.body);
+        if (!product) {
+            return res.status(404).json({message: "Oops!! Product not found"})
+            
+        } else {
+            
+            res.status(200).json({message: "Product updated successfully !"});
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message})
+        
+    }
+
+}
+//------------------------------------------------------------------------//
+//This is use show the category.//
+
+
+exports.showCategory=async(req, res)=>{
+    try {
+        const product=await category_model.find({});
+        res.status(200).json(product) //Getting all the product list.
+    } catch (error) {
+        req.status(500).json({message: error.message})
+        
+    }
+}
--- routes/category.routes.js
+++ routes/category.routes.js
@@ -9,3 +9,12 @@ auth_mw = require("../middlewares/auth.mw")
 module.exports = (app)=>{
     app.post("/ecomm/api/v1/auth/categories",[auth_mw.verifyToken, authMw.isAdmin],category_controller.createNewCategory)
 }
+module.exports = (app)=>{
+    app.delete("/ecomm/api/v1/auth/category/:id",[auth_mw.verifyToken, authMw.isAdmin],category_controller.deleteCategory)
+}
+module.exports = (app)=>{
+    app.put("/ecomm/api/v1/auth/category/:id",[auth_mw.verifyToken, authMw.isAdmin],category_controller.updateCategory)
+}
+module.exports = (app)=>{
+    app.get("/ecomm/api/v1/auth/categories",category_controller.showCategory)
+}
